45949277d1c692762d2784743b8d4b6e8ce3651d
Yehudit-Krausz <yk6749841@gmail.com>
2026-06-04 19:55:14 +0300

    Implement change audio format feature (Infrastructure and API)

---
+++ FFmpeg.Infrastructure/Commands/ChangeAudioFormatCommand.cs
@@ -0,0 +1,31 @@
+using FFmpeg.Core.Models;
+using FFmpeg.Infrastructure.Services;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Infrastructure.Commands
+{
+    public class ChangeAudioFormatCommand
+    {
+        private readonly FFmpegExecutor _executor;
+
+        public ChangeAudioFormatCommand(FFmpegExecutor executor)
+        {
+            _executor = executor;
+        }
+
+        public async Task<CommandResult> ExecuteAsync(ChangeAudioFormatModel model)
+        {
+            // פקודת ה-ffmpeg המדויקת מהמשימה שלך
+            string arguments = $"-i \"{model.InputFile}\" \"{model.OutputFile}\"";
+            
+            var (success, output, error) = await _executor.RunCommandAsync(arguments);
+            
+            return new CommandResult 
+            { 
+                IsSuccess = success, 
+                ErrorMessage = error,
+                CommandExecuted = arguments
+            };
+        }
+    }
+}
