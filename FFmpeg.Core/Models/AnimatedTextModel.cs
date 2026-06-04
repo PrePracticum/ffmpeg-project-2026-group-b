033dfad34cc6156392bf72b42cd1bb8971875b13
shoshis-git <sh0556735750@gmail.com>
2026-06-04 17:36:57 +0300

    feat: Add animated text feature with animation support

---
+++ FFmpeg.Core/Models/AnimatedTextModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Core.Models
+{
+    public enum AnimationType
+    {
+        None = 0,
+        MoveLeft = 1,
+        MoveRight = 2,
+        SlideDown = 3,
+        SlideUp = 4
+    }
+    public class AnimatedTextModel
+    {
+        public string InputFile { get; set; }
+        public string Text { get; set; }
+        public string Color { get; set; }
+        public int FontSize { get; set; }
+        public int XPosition { get; set; } = 540;
+        public int YPosition { get; set; } = 960;
+        public string OutputFile { get; set; }
+        
+        // Animation properties
+        public bool IsAnimated { get; set; } = false;
+        public AnimationType AnimationType { get; set; } = AnimationType.MoveLeft;
+        public int AnimationSpeed { get; set; } = 100; // pixels per second
+
+    }
+}
---
+++ FFmpeg.Infrastructure/Commands/AnimatedTextCommand.cs
@@ -0,0 +1,63 @@
+﻿using Ffmpeg.Command.Commands;
+using FFmpeg.Core.Models;
+using FFmpeg.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Infrastructure.Commands
+{
+    internal class AnimatedTextCommand : BaseCommand, ICommand<AnimatedTextModel>
+    {
+        private readonly ICommandBuilder _commandBuilder;
+        public AnimatedTextCommand(FFmpegExecutor executor, ICommandBuilder commandBuilder)
+            : base(executor)
+        {
+            _commandBuilder = commandBuilder ?? throw new ArgumentNullException(nameof(commandBuilder));
+        }
+
+        public async Task<CommandResult> ExecuteAsync(AnimatedTextModel model)
+        {
+            // Build x and y expressions (can be static or animated)
+            string xExpression = model.XPosition.ToString();
+            string yExpression = model.YPosition.ToString();
+
+            if (model.IsAnimated)
+            {
+                switch (model.AnimationType)
+                {
+                    case AnimationType.MoveLeft:
+                        // Text moves from right to left across screen
+                        xExpression = $"if(lt(t\\,5)\\,w+t*{model.AnimationSpeed}\\,w-50)";
+                        break;
+                    case AnimationType.MoveRight:
+                        // Text moves from left to right
+                        xExpression = $"t*{model.AnimationSpeed}";
+                        break;
+                    case AnimationType.SlideDown:
+                        // Text slides down from top
+                        yExpression = $"if(lt(t\\,5)\\,0+t*{model.AnimationSpeed}\\,h/2)";
+                        break;
+                    case AnimationType.SlideUp:
+                        // Text slides up from bottom
+                        yExpression = $"if(lt(t\\,5)\\,h-t*{model.AnimationSpeed}\\,h/2)";
+                        break;
+                }
+            }
+
+            // Build the drawtext filter with animation support
+            // Commas inside expressions must be escaped with backslash
+            string drawTextFilter = $"drawtext=text='{model.Text}':x={xExpression}:y={yExpression}:fontsize={model.FontSize}:fontcolor={model.Color}";
+
+            CommandBuilder = _commandBuilder
+                .SetInput(model.InputFile)
+                .AddOption($"-vf \"{drawTextFilter}\"")
+                .SetOutput(model.OutputFile);
+
+            return await RunAsync();
+        }
+    }
+  
+}
