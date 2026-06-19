b00228398fa2ce6f8b25af866012b052cfa73494
zehava-drabkin <z0583212849@gmail.com>
2026-06-19 15:16:18 +0300

    fix

--- FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
+++ FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
@@ -139,7 +139,7 @@ namespace FFmpeg.Infrastructure.Services
         }
         public ICommand<DuplicateVideoModel> CreateDuplicateVideoCommand()
         {
-            return new DuplicateVideoCommand(_executor, _commandBuilder());
+            return new DuplicateVideoCommand(_executor, _commandBuilder);
         }
     }
 }
